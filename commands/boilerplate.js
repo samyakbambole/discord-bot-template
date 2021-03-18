0f47fecba2e6d5bb7c10982dd2c22ca24c4e63a5
samyakbambole <samyak_study@hotmail.com>
2021-03-18 19:22:28 +0530

    :tada: Adding boilerplate.js

---
+++ commands/boilerplate.js
@@ -0,0 +1,11 @@
+// copy the contents of this file in every other file!
+
+// ---------------------------------------------------
+
+const Discord = require('discord.js'); 
+const consola = require('consola'); 
+const chalk = require('chalk'); 
+
+module.exports = async (message) => {
+     
+}
